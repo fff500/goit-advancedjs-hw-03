d828724d0e23c9517daa4896d7f90e56693ffaff
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2023-11-29 15:27:15 +0200

    Homework solution was finalised

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const elements = {
@@ -24,24 +28,37 @@ fetchBreeds()
   })
   .catch(error => {
     console.error(error);
-    elements.error.classList.remove('display-none');
+    iziToast.error({
+      message: 'Oops! Something went wrong! Try reloading the page!',
+    });
   })
   .finally(() => {
     elements.loader.classList.add('display-none');
   });
 
 function handleSelectChange(event) {
-  elements.info.classList.add('display-none');
+  elements.info.innerHTML = '';
   elements.loader.classList.remove('display-none');
 
   fetchCatByBreed(event.target.value)
     .then(data => {
-      console.log(data);
-      elements.info.classList.remove('display-none');
+      const { url, breeds } = data[0];
+      const { name, description, temperament } = breeds[0];
+
+      elements.info.innerHTML = `
+        <img class="image" src="${url}" alt="${name}" />
+        <div class="text-wrapper">
+          <h1 class="name">${name}</h1>
+          <p class="description">${description}</p>
+          <p class="temperament"><b>Temperament:</b> ${temperament}</p>
+        </div>
+      `;
     })
     .catch(error => {
       console.error(error);
-      elements.error.classList.remove('display-none');
+      iziToast.error({
+        message: 'Oops! Something went wrong! Try reloading the page!',
+      });
     })
     .finally(() => {
       elements.loader.classList.add('display-none');
